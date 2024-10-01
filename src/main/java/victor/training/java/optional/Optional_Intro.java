77200929db108430a3b4984c67b1c073cd02ab53
rafi.isaharov <rafi.isaharov@dh.com>
2024-10-01 10:17:46 +0300

    NPE - null pointer exception is very explicit from java 17

--- src/main/java/victor/training/java/optional/Optional_Intro.java
+++ src/main/java/victor/training/java/optional/Optional_Intro.java
@@ -12,13 +12,13 @@ import java.util.Map;
 public class Optional_Intro {
   public static void main(String[] args) {
     // test with 10 points or no MemberCard
-    System.out.println(getDiscountLine(new Customer(
-        new MemberCard("bar", 60))));
+    System.out.println(getDiscountLine(new Customer(new MemberCard("bar", 60))));
+  // NPE -   Of the most useful features of Java 17. In practice, the new pointers are very explicit
+    System.out.println(getDiscountLine(new Customer(new MemberCard("bar", 10))));
   }
 
   public static String getDiscountLine(Customer customer) {
-    Discount discount = computeDiscount(customer.getMemberCard());
-    return "You got a discount of %" + discount.globalPercentage();
+      return "You got a discount of %" + computeDiscount(customer.getMemberCard()).globalPercentage();
   }
 
   private static Discount computeDiscount(MemberCard card) {
