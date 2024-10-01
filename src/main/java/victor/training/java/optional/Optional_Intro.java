e4cfb1e96d7ed30f2789aa3a2f0ea2b31e193d3e
rafi.isaharov <rafi.isaharov@dh.com>
2024-10-01 14:33:09 +0300

    using Optional mono implementation created one single beautiful Functional pipeline

--- src/main/java/victor/training/java/optional/Optional_Intro.java
+++ src/main/java/victor/training/java/optional/Optional_Intro.java
@@ -26,17 +26,40 @@ public class Optional_Intro {
 
   public static String getDiscountLine(Customer customer) {
     //enjoy the new Java 17 feature, wonderful exception message of NPE
-
-    Optional<Discount> discount = computeDiscount(customer.getMemberCard());
-//    How is this better?
-//What's better is that the developers that call this method from discount had to type or orElseThrow.
+      //    How is this better?
+//What's better is that the developers that call this method from discountOptional had to type or orElseThrow.
 //Had to assume the possibility of an empty box and an exception in here something that was previously promiscuous and very mysterious.
-    //To explain the absence of something, the absence of a discount, the absence of a value.
-    if(discount.isPresent()) {
-      return "You got a discount of %" + discount.orElseThrow().globalPercentage();
-    } else {
-      return "Earn more points to get a discount";
-    }
+    //To explain the absence of something, the absence of a discountOptional, the absence of a value.
+
+//    Intellij claims that there is a simple way. There is AI can collapse all this into a single expression.
+//    if(discountOptional.isPresent()) {
+//      return "You got a discountOptional of %" + discountOptional.orElseThrow().globalPercentage();
+//    } else {
+//      return "Earn more points to get a discountOptional";
+//    }
+
+//    optional and stream is a mono implementation.
+// what is Monad? //https://dzone.com/articles/what-is-a-monad-basic-theory-for-a-java-developer
+//    A monad is a design pattern that allows you to chain operations together.
+//    It's a way to handle sequences of operations in a way that's more readable and maintainable.
+//    Monad is a concept from a part of mathematics called category theory. In the world of software, it can be implemented as a class or trait in any statically typed language
+// with generics support. Moreover, we can view it as a wrapper that puts our value in some context and allows us to perform operations,
+// specifically operations that return value wrapped in the same context, on the value.
+// Also, we can chain the operations in such a manner that the output of an operation at any step is the input to the operation at the next step.
+    // best way to use Optional is without .get or .isPresent or .orElseThrow or .orElse
+    // I want to chain operations on the value inside the Optional
+//    Optional<Integer> gpOptional = discountOptional.map(Discount::globalPercentage);
+//    Optional<String> optionalString = gpOptional.map(gp -> "You got a discountOptional of %" + gp);
+//    return optionalString.orElse("Earn more points to get a discountOptional");
+    /*    return computeDiscount(customer.getMemberCard())
+            .map(d -> "You got a discountOptional of %" + d.globalPercentage()) // only executed if the box is full
+            .orElse("Earn more points to get a discountOptional");*/
+      return computeDiscount(customer.getMemberCard())
+              .map(Discount::globalPercentage)
+              .map(percentage -> "You got a discountOptional of %" + percentage)
+              .orElse("Earn more points to get a discountOptional");
+
+
   }
 // CTRL + SHIFT + ENTER => complete the statement with the missing parts
   private static Optional<Discount> computeDiscount(MemberCard card) {
