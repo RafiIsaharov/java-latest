f2c24d1453b80cbc2d75a6c5950bf96306822653
rafi.isaharov <rafi.isaharov@dh.com>
2024-10-01 17:57:28 +0300

    fixed a bug I found due to compilation failures in legacy code

--- src/main/java/victor/training/java/optional/Biz.java
+++ src/main/java/victor/training/java/optional/Biz.java
@@ -2,6 +2,7 @@ package victor.training.java.optional;
 
 import lombok.RequiredArgsConstructor;
 import victor.training.java.optional.model.Customer;
+import victor.training.java.optional.model.MemberCard;
 import victor.training.java.optional.model.Order;
 
 @RequiredArgsConstructor
@@ -11,7 +12,11 @@ public class Biz {
    public void applyDiscount(Order order, Customer customer) {
       System.out.println("START");
       if (order.getOfferDate().before(config.getLastPromoDate())) { // TODO inside
-         int points = customer.getMemberCard().getFidelityPoints();
+         // broke compilation when discovered BUG this will return NPE so i should fix it because
+//         int points = customer.getMemberCard().getFidelityPoints();
+//         if the customer did not have a member card, the points default to 0
+         int points = customer.getMemberCard()
+                 .map(MemberCard::getFidelityPoints).orElse(0);
          order.setPrice(order.getPrice() * (100 - 2 * points) / 100);
          System.out.println("APPLIED DISCOUNT using " + customer.getMemberCard().getBarcode());
       } else {
