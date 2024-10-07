a593495c5f225a114100bd2e8153b7b5922fabf6
rafi.isaharov <rafi.isaharov@dh.com>
2024-10-07 13:02:27 +0300

    created immutability  with traditional Java

--- src/main/java/victor/training/java/records/RecordsIntro.java
+++ src/main/java/victor/training/java/records/RecordsIntro.java
@@ -1,14 +1,10 @@
 package victor.training.java.records;
 
-import lombok.Data;
-
-import java.util.Objects;
-
 public class RecordsIntro {
   public static void main(String[] args) {
-    Point point = new Point();
-    point.setX(1);
-    point.setY(1);
+    Point point = new Point(1,1);
+//    point.setX(1);
+//    point.setY(1);
     //What's wrong with setters?
 //    Well, nothing tells me that this method inside of it somewhere darker than that. It changes the state of my argument.
     darkLogic(point);
@@ -28,12 +24,53 @@ public class RecordsIntro {
 
   private static void darkerPlace(Point point) {
     //race conditions multi-threading code can happen here
-    point.setX(point.getX() + 1); // was quick fix TO DO remove on next sprint
+    // once we convert to immutable, we can't change the state of the object
+    //point.setX(point.getX() + 1); // was quick fix TO DO remove on next sprint
   }
 }
-@Data // => @ToString, @EqualsAndHashCode, @Getter, @Setter
+
+// => @ToString, @EqualsAndHashCode, @Getter, @Setter
 class Point {
-  private int x;
-  private int y;
+  private final int x;
+  private final int y;
+
+  public Point(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return this.x;
+  }
+
+  public int getY() {
+    return this.y;
+  }
+
+  public boolean equals(final Object o) {
+    if (o == this) return true;
+    if (!(o instanceof Point)) return false;
+    final Point other = (Point) o;
+    if (!other.canEqual((Object) this)) return false;
+    if (this.getX() != other.getX()) return false;
+    if (this.getY() != other.getY()) return false;
+    return true;
+  }
+
+  protected boolean canEqual(final Object other) {
+    return other instanceof Point;
+  }
+
+  public int hashCode() {
+    final int PRIME = 59;
+    int result = 1;
+    result = result * PRIME + this.getX();
+    result = result * PRIME + this.getY();
+    return result;
+  }
+
+  public String toString() {
+    return "Point(x=" + this.getX() + ", y=" + this.getY() + ")";
+  }
 }
 
