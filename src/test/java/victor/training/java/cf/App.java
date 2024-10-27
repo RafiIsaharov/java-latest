5d0ede8767888c4b71a9b45a541daa751fc34041
rafi.isaharov <rafi.isaharov@dh.com>
2024-10-27 14:54:10 +0200

    Day 4: Computable future : Fire-and-forget auditTheDrink, using CompletableFuture<Void> , how do fix that I can see exception: 4 solution: full fallback-based non-blocking concurrency. using thenCombine. Always pass an executor to any CF method ending in ...Async

--- src/test/java/victor/training/java/cf/App.java
+++ src/test/java/victor/training/java/cf/App.java
@@ -3,6 +3,7 @@ package victor.training.java.cf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -13,5 +14,16 @@ public class App {
   @Bean
   public RestTemplate restTemplate() {
     return new RestTemplate();
+  }
+
+  @Bean
+  public ThreadPoolTaskExecutor poolBar() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(20); // core == max
+    executor.setMaxPoolSize(20); // how to decide size?
+    executor.setQueueCapacity(500);// how to decide size?
+    executor.setThreadNamePrefix("bar-");
+    executor.initialize();
+    return executor;
   }
 }
