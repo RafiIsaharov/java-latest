4cbcf5fe772d46880fb198358e311ebbad9e8b1c
rafi.isaharov <rafi.isaharov@dh.com>
2024-11-05 16:17:51 +0200

    Day 4: Computable future : CPU -> idle-> unfair

--- src/test/java/victor/training/java/cf/Barman.java
+++ src/test/java/victor/training/java/cf/Barman.java
@@ -122,6 +122,7 @@ public class Barman {
     // 1:why?
     // - Starvation: because the default thread pool is shared by all the CFs in the JVM; starve others
     // ---> other flows using the default thread pool will not be impacted if you exhaust YOUR PRIVATE THREAD POOL
+    //    when(threads just sleep ) CPU -> idle-> unfair
     // - Metadata propagation: you will lose the ThreadLocal magic metadata
     //    (Logback MDC, Spring Security Context, Open Telemetry TraceID, etc)
     //  the thread pool you submit your work should be one managed by Spring/framework
